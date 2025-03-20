83440ce1b0725377279e26087b84fa990d74ce0f
belokurov_a_i <belokurovarturka@gmail.com>
2025-03-20 14:43:18 +0300

    task4

--- Task2/main.py
+++ Task2/main.py
@@ -2,15 +2,16 @@ from Task2.utils.file_utils import read_matrices_from_file, write_matrices_to_fi
 from Task2.sort_сolumns import sort_columns
 
 def main(input_filename, output_filename):
-
     matrices = read_matrices_from_file(input_filename)
 
     sorted_matrices = [sort_columns(matrix) for matrix in matrices]
 
     write_matrices_to_file(output_filename, sorted_matrices)
 
+    print(f"Данные из файла '{input_filename}' обработаны и сохранены в '{output_filename}'.")
+
 
 if __name__ == "__main__":
-    input_filename = "files/input.txt"
-    output_filename = "files/output.txt"
+    input_filename = "tests/input.txt"
+    output_filename = "tests/output.txt"
     main(input_filename, output_filename)
--- Task3/main.py
+++ Task3/main.py
@@ -4,11 +4,15 @@ from Task3.utils.file_utils import read_rectangles, write_result_to_file
 
 def main(input_filename, output_filename):
     rectangles = read_rectangles(input_filename)
+
     best_rectangle, count = find_max_containing_rectangle(rectangles)
+
     write_result_to_file(output_filename, best_rectangle, count)
+
+    print(f"Данные из файла '{input_filename}' обработаны и сохранены в '{output_filename}'.")
 
 
 if __name__ == "__main__":
-    input_filename = "files/input05.txt"
-    output_filename = "files/output.txt"
+    input_filename = "tests/input01.txt"
+    output_filename = "tests/output.txt"
     main(input_filename, output_filename)
